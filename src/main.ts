1b97b3e5d39ec9078e2059bf8287eb46e6c4249f
Nejc Susin <nejc.susin@gmail.com>
2022-02-19 15:13:38 +0100

    Added dynamic page title

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,14 @@ Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
+const DEFAULT_TITLE = 'WC III Unit Quiz';
+
+router.afterEach((to, from) => {
+  Vue.nextTick(() => {
+    document.title = `WC III Unit ${to.name} Quiz` || DEFAULT_TITLE;
+  });
+});
+
 new Vue({
   router,
   render: h => h(App)
